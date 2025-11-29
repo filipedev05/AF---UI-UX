caef5d2f9d4b4ff93dc9f4f844231536a590d55c
Filipe José de Oliveira <filipe06joseoliveira@gmail.com>
2025-11-28 21:25:23 -0300

    Add files via upload

--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -5,12 +5,21 @@ public class TesteCalculadora {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        // Testes rápidos
-        calc.calcular( 2,  3,  "+");   // esperado: 5
-        calc.calcular( 10,  4,  "-");  // esperado: 6
-        calc.calcular( 3,  5,  "*");   // esperado: 15
-        calc.calcular( 8,  2,  "/");   // esperado: 4
-        calc.calcular( 8,  0,  "/");   // divisao por zero
-        calc.calcular( 5,  5,  "x");   // operação inválida
+        System.out.println(calc.calcular( 2,  3,  "+"));
+        System.out.println(calc.calcular( 10,  4,  "-"));
+        System.out.println(calc.calcular( 3,  5,  "*"));
+        System.out.println(calc.calcular( 8,  2,  "/"));
+
+        try {
+            System.out.println(calc.calcular( 8,  0,  "/")); // exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(calc.calcular( 5,  5,  "x")); // exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
